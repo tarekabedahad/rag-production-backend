70e4a6989522fe821b03a6edf44b0e81678c4c74
tarekabedahad <tarek.abed.ahad@gmail.com>
2026-05-23 00:55:46 +0300

    feat: add basic inngest client wrapper and ingest function

--- main.py
+++ main.py
@@ -3,4 +3,29 @@ from fastapi import FastAPI
 import inngest
 import inngest.fast_api
 from inngest.experimental import ai
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
 
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+fn_id="RAG: Ingest PDF",
+trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+      return{"hello": "world "}
+
+
+app = FastAPI()
+
+
+inngest.fast_api.serve(app, inngest_client, functions=[])
